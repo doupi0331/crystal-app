cb7b9c5ea98ec2597e1697b0af216c14b994d8c6
doupi0331 <lisau4mp6@gmail.com>
2017-03-15 10:19:27 +0800

    add login api

--- app/controllers/api_v1/auth_controller.rb
+++ app/controllers/api_v1/auth_controller.rb
@@ -2,23 +2,23 @@ class ApiV1::AuthController < ApiController
   # before_action :authenticate_user!, :only => [:logout]
   
   def login
-    success = false
 
     if params[:phone] && params[:password]
       # 由手機號碼找出member
+      member = Member.find_by(phone: params[:phone])
       # 並確認member存在並且密碼正確
-      # success = user && user.valid_password?( params[:password] ) 參考
+      
+      if member && member.authenticate( params[:password] )
+        # 回傳正確訊息
+        render :json => { :message => "successed",
+                          :id => "#{member.id}"}
+      else
+        # 回傳錯誤訊息
+        render :json => { :message => "failed", 
+                          :error => "行動電話或密碼不正確" }, :status => 401
+      end
+      
     end
-
-    if success
-      # 回傳正確訊息
-      #render :json => { :auth_token => user.authentication_token,
-      #                  :user_id => user.id}
-    else
-      # 回傳錯誤訊息
-      #render :json => { :message => "email or password is not correct" }, :status => 401
-    end
-
   end
 
   def logout
--- app/controllers/api_v1/members_controller.rb
+++ app/controllers/api_v1/members_controller.rb
@@ -15,7 +15,8 @@ class ApiV1::MembersController < ApiController
 
     if @member.save
       
-      render :json => { :id =>"#{@member.id}" }
+      #render :json => { :id =>"#{@member.id}" }
+      render :json => { :message => "successed" }
     else
       
       render :json => { :message => "failed", :errors => @member.errors.full_messages.to_a }, :status => 400
